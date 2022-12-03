8512601ba7312ce7614d66e594ede564291677d2
Joshua Tan <joshuatan0806@gmail.com>
2022-12-03 13:27:18 +1030

    scripts

---
+++ GachaGods3/Assets/Scripts/StatData.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(menuName = "Character/Stat")]
+public class StatData : ScriptableObject
+{
+    public Stat Stat => stat;
+    [SerializeField] Stat stat;
+
+    public float Flat
+    {
+        get
+        {
+            return flat;
+        }
+        set
+        {
+            flat = value;
+            CalculateTotal();
+        }
+    }
+    [SerializeField] float flat = 0;
+
+    public float Percent
+    {
+        get
+        {
+            return percent;
+        }
+        set
+        {
+            percent = value;
+            CalculateTotal();
+        }
+    }
+    [SerializeField] float percent;
+
+    public float Multiplier
+    {
+        get
+        {
+            return multiplier;
+        }
+        set
+        {
+            multiplier = value;
+            CalculateTotal();
+        }
+    }
+    [SerializeField]  float multiplier = 1;
+
+    public float Total => total;
+    [ReadOnly, SerializeField] float total;
+
+    private void OnValidate()
+    {
+        CalculateTotal();
+    }
+
+    public void CalculateTotal()
+    {
+        total = ((flat + (flat * (percent / 100))) * multiplier);
+    }
+
+    public static StatData operator+ (StatData l, StatData r)
+    {
+        if (l.Stat != r.Stat)
+            throw new System.Exception("Stats are different");
+
+        StatData stat = StatManager.NullStat(l.Stat);
+
+        stat.Flat = l.Flat + r.Flat;
+        stat.Percent = l.Percent + r.Percent;
+        stat.Multiplier = l.Multiplier * r.Multiplier;
+
+        return stat;
+    }
+
+    public static StatData operator - (StatData l, StatData r)
+    {
+        if (l.Stat != r.Stat)
+            throw new System.Exception("Stats are different");
+
+        StatData stat = StatManager.NullStat(l.Stat);
+
+        stat.Flat = l.Flat - r.Flat;
+        stat.Percent = l.Percent - r.Percent;
+        stat.Multiplier = l.Multiplier * 1 / r.Multiplier;
+
+        return stat;
+    }
+
+    public void SetStat(Stat stat)
+    {
+        this.stat = stat;
+    }
+}
+
+public enum Stat
+{
+    Health,
+    Spd,
+    Damage,
+    CritChance,
+    CritDamage,
+    Deat
+}
+
+public enum StatModifier
+{
+    Flat,
+    Percent,
+    Multiplier
+}
---
+++ GachaGods3/Assets/Scripts/StatManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEditor;
+
+[CreateAssetMenu(menuName = "Managers/StatManager")]
+public class StatManager : Factories.FactoryBase
+{
+    public static StatDictionary StatDictionary => statDictionary;
+    static StatDictionary statDictionary = new StatDictionary();
+    [SerializeField, ReadOnly] StatDictionary stats;
+
+    public override void Initialise()
+    {
+        statDictionary = stats;
+    }
+
+    public static StatData NullStat(Stat stat)
+    {
+        return Instantiate(statDictionary[stat]);
+    }
+
+    public static StatData CreateStat(Stat stat, float flat = 0, float percent = 0, float multiplier = 1)
+    {
+        StatData newStat = NullStat(stat);
+
+        newStat.Flat = flat;
+        newStat.Percent = percent;
+        newStat.Multiplier = multiplier;
+   
+        return newStat;
+    }
+
+#if UNITY_EDITOR
+    [Button]
+    void DestroyStats()
+    {
+        foreach (var item in stats)
+        {
+            DestroyImmediate(item.Value, true);
+        }
+
+        stats.Clear();
+    }
+
+    [Button]
+    void CreateStats()
+    {
+        for (int i = 0; i < System.Enum.GetNames(typeof(Stat)).Length; i++)
+        {
+            if (stats.ContainsKey((Stat)i))
+                continue;
+
+            StatData stat = ScriptableObject.CreateInstance<StatData>();
+            stat.name = ((Stat)i).ToString();
+            stat.SetStat((Stat)i);
+            AssetDatabase.AddObjectToAsset(stat, this);
+            stats.Add((Stat)i, stat);
+        }
+    }
+#endif
+}
