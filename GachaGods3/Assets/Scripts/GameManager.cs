4397f85aec7cc8f399f320ecf31e1d4b4ed3ded6
Joshua Tan <joshuatan0806@gmail.com>
2022-12-03 14:20:52 +1030

    gamemanager

--- GachaGods3/Assets/Scripts/GameManager.cs
+++ GachaGods3/Assets/Scripts/GameManager.cs
@@ -6,4 +6,39 @@ public class GameManager : MonoBehaviour
 {
     public static System.Action OnGameStart;
     public static System.Action OnGameEnd;
+
+    public static System.Action OnPlanningStart;
+    public static System.Action OnPlanningEnd;
+    public static System.Action OnCombatStart;
+    public static System.Action OnCombatEnd;
+
+    public static int RoundNumber => roundNumber;
+    static int roundNumber;
+
+    private void Awake()
+    {
+        roundNumber = 0;
+        StartPlanning();
+    }
+
+    public static void StartPlanning()
+    {
+        roundNumber++;
+        OnPlanningStart?.Invoke();
+    }
+
+    public static void EndPlanning()
+    {
+        OnPlanningEnd?.Invoke();
+    }
+
+    public static void StartCombat()
+    {
+        OnCombatStart?.Invoke();
+    }
+
+    public static void EndCombat()
+    {
+        OnCombatEnd?.Invoke();
+    }
 }
