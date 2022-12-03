a17edea9a26c8e8a0c2e18e146e44732c0bea1dc
Joshua Tan <joshuatan0806@gmail.com>
2022-12-03 13:52:51 +1030

    characters

--- GachaGods3/Assets/Scripts/Character.cs
+++ GachaGods3/Assets/Scripts/Character.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 [CreateAssetMenu(menuName = "Character/Character")]
 public class Character : ScriptableObject
 {
     public Rarity Rarity => rarity;
     [SerializeField] Rarity rarity;
+
+    [Button]
+    void QWE()
+    {
+        for (int i = action.Count - 1; i >= 0; i--)
+        {
+            AssetDatabase.RemoveObjectFromAsset(action[i]);
+            Object.DestroyImmediate(action[i], true);
+        }
+
+        UnityEditor.EditorUtility.SetDirty(this);
+        UnityEditor.AssetDatabase.SaveAssets();
+    }
 }
