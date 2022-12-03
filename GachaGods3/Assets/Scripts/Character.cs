e9a7f727a8e12701e45f0a8bd399448f73a81ac5
Joshua Tan <joshuatan0806@gmail.com>
2022-12-03 13:53:40 +1030

    character

--- GachaGods3/Assets/Scripts/Character.cs
+++ GachaGods3/Assets/Scripts/Character.cs
@@ -8,17 +8,4 @@ public class Character : ScriptableObject
 {
     public Rarity Rarity => rarity;
     [SerializeField] Rarity rarity;
-
-    [Button]
-    void QWE()
-    {
-        for (int i = action.Count - 1; i >= 0; i--)
-        {
-            AssetDatabase.RemoveObjectFromAsset(action[i]);
-            Object.DestroyImmediate(action[i], true);
-        }
-
-        UnityEditor.EditorUtility.SetDirty(this);
-        UnityEditor.AssetDatabase.SaveAssets();
-    }
 }
