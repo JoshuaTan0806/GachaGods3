3c97b2c20b2f74707e4610359497de34965d1e0e
Joshua Tan <joshuatan0806@gmail.com>
2022-12-03 13:44:01 +1030

    character and game manager

---
+++ GachaGods3/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Linq;
+
+[CreateAssetMenu(menuName = "Managers/Character Manager")]
+public class CharacterManager : Factories.FactoryBase
+{
+    public static List<Character> Characters => characters;
+    static List<Character> characters = new List<Character>();
+    [SerializeField] List<Character> _characters;
+
+    public static List<Rarity> Rarities => rarities;
+    static List<Rarity> rarities = new List<Rarity>();
+    [SerializeField] List<Rarity> _rarities;
+
+    public static CharacterBlessings CharacterMastery => characterMastery;
+    static CharacterBlessings characterMastery = new CharacterBlessings();
+    public static List<Character> ActiveCharacters => activeCharacters;
+    static List<Character> activeCharacters = new List<Character>();
+  
+    public static System.Action<Character> OnCharacterPulled;
+
+    public override void Initialise()
+    {
+        GameManager.OnGameStart -= Clear;
+        GameManager.OnGameStart += Clear;
+
+        GameManager.OnGameEnd -= Clear;
+        GameManager.OnGameEnd += Clear;
+
+        characters = _characters;
+        rarities = _rarities.OrderBy(x => x.RarityNumber).ToList();
+    }
+
+    void Clear()
+    {
+        CharacterMastery.Clear();
+        ActiveCharacters.Clear();
+    }
+
+    public static void AddCharacter(Character character)
+    {
+        if (CharacterMastery.ContainsKey(character))
+        {
+            if (CharacterMastery[character] < CharacterMastery.Count)
+                CharacterMastery[character]++;
+        }
+        else
+        {
+            CharacterMastery.Add(character, 0);
+        }
+
+        OnCharacterPulled?.Invoke(character);
+    }
+
+    public static void ActivateCharacter(Character character)
+    {
+        if (activeCharacters.Contains(character))
+            throw new System.Exception("Can't add a character which is already active");
+        else
+        {
+            activeCharacters.Add(character);
+        }
+    }
+
+    public static void DeactivateCharacter(Character character)
+    {
+        if (!activeCharacters.Contains(character))
+            throw new System.Exception("Can't remove a character that is inactive");
+        else
+        {
+            activeCharacters.Remove(character);
+        }
+    }
+}
+
+public class CharacterBlessings : SerializableDictionary<Character, int> { }
---
+++ GachaGods3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static System.Action OnGameStart;
+    public static System.Action OnGameEnd;
+}
